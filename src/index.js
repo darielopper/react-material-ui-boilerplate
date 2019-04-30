c9ee298d52ca99bb910d19c13d963d0eb0f3fbf5
dramosv90 <vdariel90@gmail.com>
2019-04-30 09:53:34 -0400

    Adding button component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Button from '@material-ui/core/Button';
 
 const App = () => (
     <div>
-        <h1>Hello World, from React App!!</h1>
+        <h1>Hello World, from React App!!</h1><br />
+        <Button variant="contained" color="primary">Register</Button>
     </div>
 )
 
